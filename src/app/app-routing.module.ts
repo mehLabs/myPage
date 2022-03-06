7abbad3709367de0558fdd451eef4c8494e906d1
Hugo <hugo.sud.ste@gmail.com>
2022-03-05 22:33:31 -0300

    Esqueleto de mi web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { EmploymentHistoryComponent } from './components/employment-history/employment-history.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'navbar',component:NavBarComponent},
+  {path:'aptitudes',component:AptitudesComponent},
+  {path:'employment-history',component:EmploymentHistoryComponent},
+  {path:'home',component:HomeComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'social-media',component:SocialMediaComponent},
+  {path:'',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { EmploymentHistoryComponent } from './components/employment-history/employment-history.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProfileComponent,
+    AptitudesComponent,
+    EmploymentHistoryComponent,
+    SocialMediaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
