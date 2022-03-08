4a01caafbb2b376262630500a633639afceac4f6
Hugo <hugo.sud.ste@gmail.com>
2022-03-08 16:55:34 -0300

    BARRA EN PROCESO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,35 @@ import { Component, ViewEncapsulation } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hugoiturrieta';
+export class AppComponent implements OnInit {
+  leftColumn:Element | null = null;
+  title:string = 'hugoiturrieta';
+  wider:boolean=false;
+
+
+  ngOnInit(): void {
+    
+   this.leftColumn = document.querySelector("#nav-column");
+
+
+   
+
+  }
+  toggleSidebarWidth(){
+    console.log(this.leftColumn);
+    if(this.wider==false && this.leftColumn != null){
+      this.leftColumn.classList.remove("narrow");
+      this.leftColumn.classList.add("wider");
+      this.wider=true;
+    }else if(this.wider==true && this.leftColumn != null){
+      this.leftColumn.classList.add("narrow");
+      this.leftColumn.classList.remove("wider");
+      this.wider=false;
+    }
+
+
+  }
 }
+
+
+  
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  @Output() sidebarClick = new EventEmitter();
+  status:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  onClick(){
+    console.log("EMIT");
+    this.sidebarClick.emit();
+    this.status = !this.status;
 
+
+  }
 }
